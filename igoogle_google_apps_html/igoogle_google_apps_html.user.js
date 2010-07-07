26a415825408bfa42a849fea3d7dd5ca482c714d
Tobbe Lundberg <tobbesweden@gmail.com>
2010-07-07 06:08:45 -0400

    Removed more unused stuff and commented the code.

--- igoogle_google_apps_html/igoogle_google_apps_html.user.js
+++ igoogle_google_apps_html/igoogle_google_apps_html.user.js
@@ -6,20 +6,22 @@
 // @include        https://mail.google.com/a/*/h/*
 // ==/UserScript==
 
+// Top bar, right half
 var elm = document.getElementById('guser');
 elm.parentNode.removeChild(elm);
 
+// Top bar, left half
 elm = document.getElementById('gbar');
 elm.parentNode.removeChild(elm);
 
+// Search and logo
 elm = document.getElementsByTagName('table')[0];
 elm.parentNode.removeChild(elm);
 
-elm = document.getElementsByClassName('bn')[0];
-elm.parentNode.removeChild(elm);
-
-elm = document.getElementsByClassName('ft')[0].getElementsByTagName('tr')[2];
-elm.parentNode.removeChild(elm);
-
+// Navigation links (left column)
 elm = document.getElementsByTagName('table')[0].childNodes[1].firstChild.childNodes[1];
+elm.parentNode.removeChild(elm);
+
+// Bottom info text
+elm = document.getElementsByTagName('table')[5];
 elm.parentNode.removeChild(elm);
